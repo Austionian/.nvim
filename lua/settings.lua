3cab96f91250c69b66c484bdf22d535f5c21b01f
Austin Rooks <Austionian@users.noreply.github.com>
2022-12-21 08:58:12 -0600

    set up undo tree

--- lua/settings.lua
+++ lua/settings.lua
@@ -13,6 +13,11 @@ vim.o.smartindent = true
 
 vim.o.colorcolumn = "80"
 
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
 vim.o.hlsearch = false
 vim.o.incsearch = true
 
