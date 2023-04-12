a9ab68ebf13fcfd292d67056895c5ef98d08577a
Austin Rooks <Austionian@users.noreply.github.com>
2023-04-12 11:37:09 -0500

    remove bg

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -126,5 +126,5 @@ vim.cmd[[colorscheme tokyonight-night]]
 --
 --
 -- REMOVE NVIM BACKGROUND
-vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
-vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
+-- vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
+-- vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
